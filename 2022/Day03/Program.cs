ea338bc06767f812669f44239872e3be4af5854f
Léon Bouquiet <Leon.Bouquiet@infosupport.com>
2022-12-03 13:32:58 +0100

    Added Day03 skeleton.

---
+++ 2022/Day03/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day03
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Part1();
+			Part2();
+
+			Console.WriteLine("Press enter to exit...");
+			Console.ReadLine();
+		}
+
+		private static void Part1()
+		{
+			int? result = 0;
+
+			Console.WriteLine($"The result of part 1 is: {result}");
+		}
+
+		private static void Part2()
+		{
+			int? result = 0;
+
+			Console.WriteLine($"The result of part 2 is: {result}");
+		}
+
+		private static IEnumerable<string> ReadInput()
+		{
+			using (Stream? stream = typeof(Program).Assembly.GetManifestResourceStream($"{typeof(Program).Namespace}.Input.txt"))
+			{
+				using (StreamReader sr = new StreamReader(stream!))
+				{
+					string? line;
+					do
+					{
+						line = sr.ReadLine();
+						if (line != null)
+							yield return line;
+					}
+					while (line != null);
+				}
+			}
+		}
+	}
+}
