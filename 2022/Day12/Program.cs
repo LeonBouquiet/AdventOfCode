53c7324684855d26871e3e584611733d27eb2489
Léon Bouquiet <Leon.Bouquiet@infosupport.com>
2022-12-21 09:45:59 +0100

    Day 12 input parsing complete.

---
+++ 2022/Day12/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Shared;
+
+namespace Day12
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Grid<int> heightmap = new Grid<int>(InputReader.Read<Program>()
+				.Select(line => line
+					.Select(c => (c - 'a')))
+				.ToList());
+			heightmap.FormatCell = (i => "" + (char)('a' + i));
+
+			Location start = heightmap.Locations.Where(loc => heightmap[loc] == ('S' - 'a')).First();
+			heightmap[start] = 0;
+			Location end = heightmap.Locations.Where(loc => heightmap[loc] == ('E' - 'a')).First();
+			heightmap[end] = 'z' - 'a';
+
+			Part1();
+			Part2();
+		}
+
+		private static void Part1()
+		{
+			var result = InputReader.Read<Program>();
+
+			Console.WriteLine($"The result of part 1 is: {result}");
+		}
+
+		private static void Part2()
+		{
+			var result = InputReader.Read<Program>();
+
+			Console.WriteLine($"The result of part 2 is: {result}");
+		}
+
+	}
+}
