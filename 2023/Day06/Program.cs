8aa57831e7a79d72e4064212950eaf892fe31f75
Léon Bouquiet <Leon.Bouquiet@infosupport.com>
2023-12-06 09:38:07 +0100

    Day06 part 1.

---
+++ 2023/Day06/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Shared;
+
+namespace Day06
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Part1();
+			Part2();
+		}
+
+		private static void Part1()
+		{
+			List<int[]> timesAndDistances = InputReader.Read<Program>()
+				.Select(line => line
+					.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+					.Skip(1)
+					.Select(s => Int32.Parse(s))
+					.ToArray())
+				.ToList();
+
+			int result = 1;
+			for(int raceIndex = 0; raceIndex < timesAndDistances[0].Length; raceIndex++)
+			{
+				int time = timesAndDistances[0][raceIndex];
+				int recordDistance = timesAndDistances[1][raceIndex];
+
+				int winCount = 0;
+				for (int holdButtonTime = 0; holdButtonTime < time; holdButtonTime++)
+				{
+					int distance = holdButtonTime * (time - holdButtonTime);
+					if (distance > recordDistance)
+						winCount++;
+				}
+
+				result *= winCount;
+			}
+
+			Console.WriteLine($"The result of part 1 is: {result}");
+		}
+
+		private static void Part2()
+		{
+			var result = InputReader.Read<Program>();
+
+			Console.WriteLine($"The result of part 2 is: {result}");
+		}
+
+	}
+}
