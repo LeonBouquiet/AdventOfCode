a75f5d2a122c1b615095656e3cf837317a680103
Léon Bouquiet <Leon.Bouquiet@infosupport.com>
2023-12-07 11:19:47 +0100

    Day07, still buggy.

--- 2023/Day07/Program.cs
+++ 2023/Day07/Program.cs
@@ -10,7 +10,7 @@ namespace Day07
 {
 	public enum Card
 	{
-		C2 = 2, C3, C4, C5, C6, C7, C8, C9, CT, CJ, CQ, CK, CA
+		CX = 1, C2, C3, C4, C5, C6, C7, C8, C9, CT, CJ, CQ, CK, CA
 	}
 
 	public enum Strength
@@ -39,12 +39,75 @@ namespace Day07
 			Strength = DetermineStrength();
 		}
 
+		public Hand Clone()
+		{
+			return new Hand(Cards.ToArray(), Bid) { Strength = this.Strength };
+		}
+
 		public static Hand Parse(string line)
 		{
 			string[] parts = line.Split(' ', StringSplitOptions.TrimEntries);
 			return new Hand(
 				parts[0].ToCharArray().Select(c => Enum.Parse<Card>("C" + c)).ToArray(),
 				Int32.Parse(parts[1]));
+		}
+
+		public static Hand ParseWithJokers(string line)
+		{
+			string[] parts = line.Split(' ', StringSplitOptions.TrimEntries);
+			Hand hand = new Hand(
+				parts[0].Replace('J', 'X').ToCharArray().Select(c => Enum.Parse<Card>("C" + c)).ToArray(),
+				Int32.Parse(parts[1]));   //Joker is represented by an 'X'.
+
+			List<Hand> permutations = hand.GenerateAllPermutationUsingJokers().ToList();
+			if (permutations.Count > 0)
+			{
+				//Use the Strenght of the best permutation, but the Cards with the Jokers.
+				permutations.Sort();
+				hand.Strength = permutations.Last().Strength;
+			}
+
+			return hand;
+		}
+
+		private IEnumerable<Hand> GenerateAllPermutationUsingJokers()
+		{
+			Hand instance = this.Clone();
+			for(int cardIndex = 0; cardIndex < Cards.Length; cardIndex++)
+			{
+				if (instance.Cards[cardIndex] == Card.CX)
+					instance.Cards[cardIndex] = Card.C2;
+			}
+
+			bool performedIncrement;
+			do
+			{
+				performedIncrement = false;
+
+				for (int cardIndex = Cards.Length - 1; cardIndex >= 0; cardIndex--)
+				{
+					if (this.Cards[cardIndex] == Card.CX)
+					{
+						if (instance.Cards[cardIndex] < Card.CA)
+						{
+							//Current cardIndex can still be incremented.
+							instance.Cards[cardIndex] = instance.Cards[cardIndex] + 1;
+							if (instance.Cards[cardIndex] == Card.CJ)
+								instance.Cards[cardIndex] = Card.CQ;	//Skip the J, doesn't exist here.
+
+							instance.Strength = instance.DetermineStrength();
+							performedIncrement = true;
+
+							yield return instance.Clone();
+						}
+						else
+						{
+							//Reset current index back to C2 and move on to the next joker position, if any.
+							instance.Cards[cardIndex] = Card.C2;
+						}
+					}
+				}
+			} while (performedIncrement);
 		}
 
 		private Strength DetermineStrength()
@@ -80,7 +143,8 @@ namespace Day07
 				if (this.Strength != other.Strength)
 					return this.Strength - other.Strength;
 
-				for(int index = 0; index < Cards.Length; index++)
+				bool includesJokers = this.Cards.Any(c => c == Card.CX) || other.Cards.Any(c => c == Card.CX);
+				for (int index = 0; index < Cards.Length; index++)
 				{
 					if (this.Cards[index] != other.Cards[index])
 						return this.Cards[index] - other.Cards[index];
@@ -102,7 +166,7 @@ namespace Day07
 	{
 		public static void Main(string[] args)
 		{
-			Part1();
+			//Part1();
 			Part2();
 		}
 
@@ -123,15 +187,19 @@ namespace Day07
 
 		private static void Part2()
 		{
-			var result = InputReader.Read<Program>();
+			List<Hand> hands = InputReader.Read<Program>()
+				.Select(line => Hand.ParseWithJokers(line))
+				.ToList();
 
+			hands.Sort();
+
+			long result = 0;
+			for (int index = 0; index < hands.Count; index++)
+				result += (index + 1) * hands[index].Bid;
+
+			//False: 248118448 (too low)
+			//False: 248497077 (too high)
 			Console.WriteLine($"The result of part 2 is: {result}");
 		}
-
-		private static Card ParseCard(char c)
-		{
-			return Enum.Parse<Card>("C" + c);
-		}
-
 	}
 }
