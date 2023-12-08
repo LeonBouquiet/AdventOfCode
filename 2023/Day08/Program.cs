193bb00b90f650f5831cfa5b4171526837795f4e
Léon Bouquiet <Leon.Bouquiet@infosupport.com>
2023-12-08 20:15:16 +0100

    Day 08 part 2, naive implementation.

--- 2023/Day08/Program.cs
+++ 2023/Day08/Program.cs
@@ -23,6 +23,14 @@ namespace Day08
 			return new Node() { Name = parts[0], Left = parts[1], Right = parts[2] };
 		}
 
+		public string GetNodeNameByDirection(char direction)
+		{
+			if (direction == 'L')
+				return Left;
+			else
+				return Right;
+		}
+
 		public override string ToString()
 		{
 			return $"{Name} = ({Left}, {Right})";
@@ -60,13 +68,9 @@ namespace Day08
 			for(; step < 10000000; step++)
 			{
 				Node current = nodeMap[currentNodeName];
-				Console.WriteLine($"Step {step + 1}: Current node is {current}, moving {directions[step % directions.Count]}...");
+				//Console.WriteLine($"Step {step + 1}: Current node is {current}, moving {directions[step % directions.Count]}...");
 
-				if (directions[step % directions.Count] == 'L')
-					currentNodeName = current.Left;
-				else
-					currentNodeName = current.Right;
-
+				currentNodeName = current.GetNodeNameByDirection(directions[step % directions.Count]);
 				if (currentNodeName == "ZZZ")
 					break;
 			} 
@@ -76,8 +80,46 @@ namespace Day08
 
 		private static void Part2()
 		{
-			var result = InputReader.Read<Program>();
+			List<string>[] sections = InputReader.Read<Program>().Concat(new string[] { "" })
+				.PartitionIntoRangesBy(line => string.IsNullOrWhiteSpace(line), includeDelimiters: false)
+				.ToArray();
 
+			List<char> directions = sections[0].First()
+				.ToCharArray()
+				.ToList();
+			List<Node> nodes = sections[1]
+				.Select(line => Node.Parse(line))
+				.ToList();
+
+			Dictionary<string, Node> nodeMap = nodes
+				.ToDictionary(node => node.Name, StringComparer.OrdinalIgnoreCase);
+
+			List<string> currentNodeNames = nodes
+				.Where(n => n.Name.EndsWith('A'))
+				.Select(n => n.Name)
+				.ToList();
+
+			int step = 0;
+			for (; step < 1000000000; step++)
+			{
+				int endingNodeCount = 0;
+				for(int index = 0; index < currentNodeNames.Count; index++)
+				{
+					Node current = nodeMap[currentNodeNames[index]];
+					currentNodeNames[index] = current.GetNodeNameByDirection(directions[step % directions.Count]);
+
+					if (currentNodeNames[index].EndsWith('Z'))
+						endingNodeCount++;
+				}
+
+				if (endingNodeCount > 2)
+					Console.WriteLine($"Step {step + 1}: Reached {endingNodeCount} ending nodes, at positions: ({string.Join(", ", currentNodeNames)}) ");
+				if (endingNodeCount == currentNodeNames.Count)
+					break;
+			}
+
+
+			var result = 0;
 			Console.WriteLine($"The result of part 2 is: {result}");
 		}
 
