b083e67217c3974ddd064a6e8cfe41df74e43c31
Léon Bouquiet <Leon.Bouquiet@infosupport.com>
2023-12-11 20:17:49 +0100

    Day 09 part 2 complete.

--- 2023/Day09/Program.cs
+++ 2023/Day09/Program.cs
@@ -26,21 +26,14 @@ namespace Day09
 			int result = 0;
 			foreach(List<int> historyLine in historyLines)
 			{
-				int extrapolated = Extrapolate(historyLine);
+				int extrapolated = ExtrapolateLast(historyLine);
 				result += extrapolated;
 			}
 
 			Console.WriteLine($"The result of part 1 is: {result}");
 		}
 
-		private static void Part2()
-		{
-			var result = InputReader.Read<Program>();
-
-			Console.WriteLine($"The result of part 2 is: {result}");
-		}
-
-		private static int Extrapolate(List<int> input)
+		private static int ExtrapolateLast(List<int> input)
 		{
 			List<int> child = new List<int>(input.Count - 1);
 			for (int index = 0; index < input.Count - 1; index++)
@@ -49,7 +42,7 @@ namespace Day09
 			if (child.All(i => i == 0) == false)
 			{
 				//Recursively explore further
-				Extrapolate(child);
+				ExtrapolateLast(child);
 			}
 			else
 			{
@@ -61,6 +54,49 @@ namespace Day09
 			//After the recursive call, the child list now has an extra extrapolated value.
 			input.Add(input[input.Count - 1] + child[child.Count - 1]);
 			return input[input.Count - 1];
+		}
+
+		private static void Part2()
+		{
+			List<List<int>> historyLines = InputReader.Read<Program>()
+				.Select(line => line
+					.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+					.Select(s => Int32.Parse(s))
+					.ToList())
+				.ToList();
+
+			int result = 0;
+			foreach (List<int> historyLine in historyLines)
+			{
+				int extrapolated = ExtrapolateFirst(historyLine);
+				result += extrapolated;
+			}
+
+			Console.WriteLine($"The result of part 2 is: {result}");
+
+		}
+
+		private static int ExtrapolateFirst(List<int> input)
+		{
+			List<int> child = new List<int>(input.Count - 1);
+			for (int index = 0; index < input.Count - 1; index++)
+				child.Add(input[index + 1] - input[index]);
+
+			if (child.All(i => i == 0) == false)
+			{
+				//Recursively explore further
+				ExtrapolateFirst(child);
+			}
+			else
+			{
+				//Found the all-zero's line, prepend a zero placeholder.
+				child.Insert(0, 0);
+				return child[0];
+			}
+
+			//After the recursive call, the child list now has an extra extrapolated value at the start
+			input.Insert(0, input[0] - child[0]);
+			return input[0];
 		}
 	}
 }
