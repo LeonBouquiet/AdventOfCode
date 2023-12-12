c1c60518361d00d5cdcd71d937d42db75805dda6
Léon Bouquiet <Leon.Bouquiet@infosupport.com>
2023-12-12 15:26:11 +0100

    Day 11 part 2.

--- 2023/Day11/Program.cs
+++ 2023/Day11/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 using Shared;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Day11
 {
@@ -82,15 +83,83 @@ namespace Day11
 					image.InsertColumn(x++, 0);
 			}
 
+			//Get all galaxy locations, and renumber them for easy reference
+			List<Location> galaxyLocations = image.Locations
+				.Where(loc => image[loc] != 0)
+				.ToList();
+			
+			int id = 1;
+			foreach (Location loc in galaxyLocations)
+				image[loc] = id++;
+
+			//Enumerate all pairs:
+			long result = 0;
+			for(int first = 0; first < galaxyLocations.Count; first++)
+			{
+				for (int second = 0; second != first && second < galaxyLocations.Count; second++)
+				{
+					int shortestPath = Math.Abs(galaxyLocations[first].X - galaxyLocations[second].X) + 
+									   Math.Abs(galaxyLocations[first].Y - galaxyLocations[second].Y);
+					result += shortestPath;
+				}
+			}
+
 			Console.WriteLine(image);
 
-			var result = 0;
 			Console.WriteLine($"The result of part 1 is: {result}");
 		}
 
 		private static void Part2()
 		{
-			var result = InputReader.Read<Program>();
+			Image image = new Image(
+				InputReader.Read<Program>()
+				.Select(line => line
+					.AsEnumerable()
+					.Select(c => (c == '#') ? 1 : 0))
+			.ToList());
+
+			//Find all empty rows
+			List<int> emptyRowIndexes = new List<int>();
+			for (int y = 0; y < image.Height; y++)
+			{
+				if (image.GetLocationsInRow(y).All(loc => image[loc] == 0))
+					emptyRowIndexes.Add(y);
+			}
+
+			//Find all empty columns
+			List<int> emptyColumnIndexes = new List<int>();
+			for (int x = 0; x < image.Width; x++)
+			{
+				if (image.GetLocationsInColumn(x).All(loc => image[loc] == 0))
+					emptyColumnIndexes.Add(x);
+			}
+
+			//Get all galaxy locations
+			List<Location> gLocs = image.Locations
+				.Where(loc => image[loc] != 0)
+				.ToList();
+
+			//Enumerate all pairs:
+			long result = 0;
+			for (int first = 0; first < gLocs.Count; first++)
+			{
+				for (int second = 0; second != first && second < gLocs.Count; second++)
+				{
+					int shortestPathLength = 0;
+
+					for (int x = Math.Min(gLocs[first].X, gLocs[second].X); x < Math.Max(gLocs[first].X, gLocs[second].X); x++)
+					{
+						shortestPathLength += (emptyColumnIndexes.Contains(x)) ? 1000000 : 1;
+					}
+
+					for (int y = Math.Min(gLocs[first].Y, gLocs[second].Y); y < Math.Max(gLocs[first].Y, gLocs[second].Y); y++)
+					{
+						shortestPathLength += (emptyRowIndexes.Contains(y)) ? 1000000 : 1;
+					}
+
+					result += shortestPathLength;
+				}
+			}
 
 			Console.WriteLine($"The result of part 2 is: {result}");
 		}
